b0ba0ce50ed0caa548b9912afcf94f1d9c399409
Jesús Pérez <jesus-perez95@hotmail.com>
2023-02-07 15:39:47 -0300

    [Pol] Flavour stuff, some refactor to improve wall collisioning

--- Assets/Scripts/Entities/Propeller.cs
+++ Assets/Scripts/Entities/Propeller.cs
@@ -8,7 +8,7 @@ namespace GalaxyOfCircles.Entities
     {
         [SerializeField] private float _movementSpeed;
         [Range(0,0.9f)]
-        [SerializeField] private float _randomOffsetBounce = 0.5f;
+        [SerializeField] private float _randomOffsetBounce = 0.3f;
         private Rigidbody2D _rBody;
         private ColorChanger _colorChanger;
         private ParticleActivator _particleActivator;
@@ -19,7 +19,8 @@ namespace GalaxyOfCircles.Entities
         private Vector2 _reflectDirection;
         private float _randomAngle;
         private float _angleMotion = 45f;
-       
+        private const string WALL_TAG = "Wall";
+        private const float MIN_TRESHOLD_MAGNITUDE = 0.9f;
 
         private void Awake()
         {
@@ -44,26 +45,40 @@ namespace GalaxyOfCircles.Entities
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            // Bounce off using Reflect function with a little bit of randomization offset.
-            _contact = collision.GetContact(0);
-            _reflectDirection = Vector2.Reflect(_moveDirection, _contact.normal);
-            _moveDirection = RandomizeDirectionOffset(-_randomOffsetBounce, _randomOffsetBounce);
-            
+            BounceDirection(collision.GetContact(0));
             ChangeColor();
-
-            // Flavour stuff
             ActivateParticles();
             ActivateGraphicBounce();
         }
 
-        private Vector2 RandomizeDirectionOffset(float minRange, float maxRange)
+        // Trying to get them away from wall as much as possible.
+        private void OnCollisionStay2D(Collision2D collision)
         {
-            _randomAngle = Random.Range(minRange, maxRange);
-            _reflectDirection.x += _randomAngle;
-            _reflectDirection.y += _randomAngle;
-            return _reflectDirection.normalized;
+            if (_rBody.velocity.magnitude > MIN_TRESHOLD_MAGNITUDE || collision.gameObject.CompareTag(WALL_TAG) == false)
+                return;
+            BounceDirection(collision.GetContact(0));
+
         }
 
+        // Bounce off using Reflect function
+        private void BounceDirection(ContactPoint2D contact)
+        {
+            _contact = contact;
+            _reflectDirection = Vector2.Reflect(_moveDirection, _contact.normal);
+            _reflectDirection = RandomizeDirectionOffset(_reflectDirection, -_randomOffsetBounce, _randomOffsetBounce); 
+            _moveDirection = _reflectDirection.normalized;
+        }
+
+        // With a little bit of randomization offset.
+        private Vector2 RandomizeDirectionOffset(Vector2 vector, float minRange, float maxRange)
+        {
+            _randomAngle = Random.Range(minRange, maxRange);
+            vector.x += _randomAngle;
+            vector.y += _randomAngle;
+            return vector;
+        }
+
+        // Flavour stuff
         private void ActivateGraphicBounce()
         {
             if (_graphicBouncer != null)
