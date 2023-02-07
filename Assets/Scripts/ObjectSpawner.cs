3e2090ca558c9d61b281d43ef99e1bf99aa39087
Jesús Pérez <jesus-perez95@hotmail.com>
2023-02-07 00:59:10 -0300

    [Add] GUI implementation

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CircleSpawner : MonoBehaviour
+public class ObjectSpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject _objectPrefab;
+    
+
+    private void Awake()
+    {
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
